86bdfe3d25347242f64b9626437be06372ccf2d8
n1n5 <nina.zukic.20@size.ba>
2025-08-27 13:54:07 +0200

    Bug fix

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -10,29 +10,27 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <body>
-                <div className="mx-auto flex max-w-screen-md justify-between lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
-                    <div className="px-2 xs:px-4 2xl:px-8">
-                        <LeftBar />
-                    </div>
-                    <div className="flex-1 border-x-[1px] border-borderCustom lg:min-w-[600px]">
-                        <div>
-                            <div className="flex justify-around border-b-[1px] border-borderCustom px-4 pt-4 font-bold text-textCustom">
-                                <Link href="/" className="flex cursor-pointer items-center border-b-4 border-iconBlue pb-3">
-                                    For you
-                                </Link>
-                                <Link href="/" className="flex cursor-pointer items-center pb-3">
-                                    Following
-                                </Link>
-                            </div>
-                            <Feed />
+            <div className="mx-auto flex max-w-screen-md justify-between lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+                <div className="px-2 xs:px-4 2xl:px-8">
+                    <LeftBar />
+                </div>
+                <div className="flex-1 border-x-[1px] border-borderCustom lg:min-w-[600px]">
+                    <div>
+                        <div className="flex justify-around border-b-[1px] border-borderCustom px-4 pt-4 font-bold text-textCustom">
+                            <Link href="/" className="flex cursor-pointer items-center border-b-4 border-iconBlue pb-3">
+                                For you
+                            </Link>
+                            <Link href="/" className="flex cursor-pointer items-center pb-3">
+                                Following
+                            </Link>
                         </div>
-                    </div>
-                    <div className="ml-4 hidden flex-1 md:ml-8 lg:flex">
-                        <RightBar />
+                        <Feed />
                     </div>
                 </div>
-            </body>
+                <div className="ml-4 hidden flex-1 md:ml-8 lg:flex">
+                    <RightBar />
+                </div>
+            </div>
         </>
     );
 }
